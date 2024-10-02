afba8bdf42e4bc6488af33bcecda78af8f7e4ffa
JieyuZ2 <jieyuzhang97@gmail.com>
2024-10-02 15:20:45 -0700

    fix bus

--- test_env.sh
+++ test_env.sh
@@ -1,6 +1,7 @@
 
 # check python
 conda deactivate
+export PATH="/opt/conda/bin:$PATH"
 echo "Conda environment deactivated. Current Python version is: $(which python)"
 echo "Python version: $(python --version)"
 pip list
