c23081cae2c783dd04432d9866e23db67ee685dd
Weikai <hwk18105962347@gmail.com>
2024-10-02 16:55:33 -0700

    fix resume

--- train_10_2.sh
+++ train_10_2.sh
@@ -20,7 +20,7 @@ echo "Python version: $(python --version)"
 # start training
 python train_net.py --dist-url 'tcp://127.0.0.1:50163' \
     --num-gpus 8 \
+    --resume \
     --config-file configs/ade20k/swin/oneformer_swin_large_bs16_160k.yaml \
     OUTPUT_DIR /results/ade20k_swin_large WANDB.NAME ade20k_swin_large \
-    --resume
 
